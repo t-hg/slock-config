cf92e9ac009fbff0ad1d5b7604c4b65501785bca
Tobias Hort-Giess <tobias.hortgiess@gmail.com>
2023-10-06 06:59:24 +0200

    clone depth

--- apply-patches.sh
+++ apply-patches.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e 
-git clone https://git.suckless.org/slock
+git clone --depth 1 https://git.suckless.org/slock
 pushd slock
 git apply ../fix-arch-linux-compilation.patch
 git apply ../dracula-theme.patch
